bafef087ced6ef59ff46288b999d30473fa72858
Marco Barbosa <marcobarbosa@outlook.com>
2024-02-13 21:32:49 +0100

    add some plugins

--- nvim/.config/nvim/lua/barbosa/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/barbosa/plugins/nvim-treesitter.lua
@@ -1,4 +1,20 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+			ensure_installed = { "html", "javascript", "typescript", "c_sharp", "c", "lua", "vim", "vimdoc", "query"},
+			sync_install = false,
+			auto_install = true,
+			highlight = { 
+				enable = true 
+			},
+			indent = { 
+				enable = true 
+			},
+			additional_vim_regex_highlighting = false,
+		})
+	end
 }
