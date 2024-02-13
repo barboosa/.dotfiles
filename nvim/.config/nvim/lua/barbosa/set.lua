21fdb70ad273278d80f30c7163156878ce22459f
Marco Barbosa <marcobarbosa@outlook.com>
2024-02-13 23:36:11 +0100

    hopefully removed history from pushing to git

--- nvim/.config/nvim/lua/barbosa/set.lua
+++ nvim/.config/nvim/lua/barbosa/set.lua
@@ -11,7 +11,7 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
-vim.opt.undodir = "~/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
