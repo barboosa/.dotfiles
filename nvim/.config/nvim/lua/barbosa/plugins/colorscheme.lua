a1ae2c4748e134cddc6ec2f04e23f1f8f8c18f22
Marco Barbosa <marcobarbosa@outlook.com>
2024-02-13 15:42:46 +0100

    better for my eyes now

--- nvim/.config/nvim/lua/barbosa/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/barbosa/plugins/colorscheme.lua
@@ -1,6 +1,9 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {},
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd("colorscheme tokyonight")
+		end
+	}
 }
