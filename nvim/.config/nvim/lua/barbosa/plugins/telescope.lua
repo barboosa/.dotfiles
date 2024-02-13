d2fe1d16678a01e45316f3da7e452c51beeef36e
Marco Barbosa <marcobarbosa@outlook.com>
2024-02-13 15:21:17 +0100

    setup neovim plugins and some first keymaps

---
+++ nvim/.config/nvim/lua/barbosa/plugins/telescope.lua
@@ -0,0 +1,18 @@
+return {
+	'nvim-telescope/telescope.nvim',
+
+	dependencies = {
+		'nvim-lua/plenary.nvim'
+	},
+
+	config = function()
+		require('telescope').setup({})
+
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+		vim.keymap.set('n', '<C-f>', builtin.git_files, {})
+		vim.keymap.set('n', '<leader>fs', function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
+	end
+}
