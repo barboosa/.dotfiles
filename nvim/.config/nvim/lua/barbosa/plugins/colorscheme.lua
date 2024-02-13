3b041174de068b954e3951866946bd974aece312
Marco Barbosa <marcobarbosa@outlook.com>
2024-02-13 22:44:14 +0100

    beautiful wow

--- nvim/.config/nvim/lua/barbosa/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/barbosa/plugins/colorscheme.lua
@@ -3,7 +3,33 @@ return {
 		"folke/tokyonight.nvim",
 		priority = 1000,
 		config = function()
+			require("tokyonight").setup({
+				style = "night",
+			})
 			vim.cmd("colorscheme tokyonight")
+		end
+	},
+	{
+		"rose-pine/neovim",
+		priority = 1000,
+		config = function()
+			require("rose-pine").setup({
+				variant = "main",
+			})
+			--vim.cmd("colorscheme rose-pine")
+		end
+	},
+	{
+		"catppuccin/nvim",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				background = {
+					dark = "mocha",
+				},
+			})
+			--vim.cmd.colorscheme "catppuccin"
 		end
 	}
 }
