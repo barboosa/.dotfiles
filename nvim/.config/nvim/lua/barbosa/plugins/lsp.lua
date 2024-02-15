fd42512489f276bb6909bef80f0f833214c1b689
Marco Barbosa <marcobarbosa@outlook.com>
2024-02-15 15:28:21 +0100

    cmp

--- nvim/.config/nvim/lua/barbosa/plugins/lsp.lua
+++ nvim/.config/nvim/lua/barbosa/plugins/lsp.lua
@@ -14,21 +14,33 @@ return {
             require('mason').setup({})
             require('mason-lspconfig').setup({
                 ensure_installed = {
-                    "omnisharp",
+                    "omnisharp", "lua_ls", "tsserver", "jsonls", "html", "cssls", "rust_analyzer", "pyright", "gopls"
                 },
 
                 handlers = {
                     lsp_zero.default_setup,
-                    lua_ls = function()
-                        require('lspconfig').lua_ls.setup({})
-                        require('lspconfig').omnisharp.setup({})
-                    end,
                 },
             })
         end
     },
     {'neovim/nvim-lspconfig'},
     {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
+    {
+        'hrsh7th/nvim-cmp',
+        config = function()
+            local cmp = require('cmp')
+            local cmp_action = require('lsp-zero').cmp_action()
+            cmp.setup({
+                mapping = cmp.mapping.preset.insert ({
+                    ['<CR>'] = cmp.mapping.confirm({select = false}),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                })
+            })
+        end
+    },
     {'L3MON4D3/LuaSnip'}
 }
