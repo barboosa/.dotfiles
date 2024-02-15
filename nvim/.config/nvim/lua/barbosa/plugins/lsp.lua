394078017d69021ce8300d4117ef239b1caa6023
Marco Barbosa <marcobarbosa@outlook.com>
2024-02-15 22:48:22 +0100

    this lsp stuff is still not working quite right

--- nvim/.config/nvim/lua/barbosa/plugins/lsp.lua
+++ nvim/.config/nvim/lua/barbosa/plugins/lsp.lua
@@ -25,6 +25,8 @@ return {
     },
     {'neovim/nvim-lspconfig'},
     {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-path'},
+    {'hrsh7th/cmp-buffer'},
     {
         'hrsh7th/nvim-cmp',
         config = function()
@@ -32,13 +34,19 @@ return {
             local cmp_action = require('lsp-zero').cmp_action()
             cmp.setup({
                 mapping = cmp.mapping.preset.insert ({
-                    ['<CR>'] = cmp.mapping.confirm({select = false}),
+                    ['<C-ll>'] = cmp.mapping.confirm({select = false}),
                     ['<C-Space>'] = cmp.mapping.complete(),
                     ['<C-f>'] = cmp_action.luasnip_jump_forward(),
                     ['<C-b>'] = cmp_action.luasnip_jump_backward(),
                     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                })
+                }),
+                sources = {
+                    {name = 'nvim_lsp'},
+                    {name = 'path'},
+                    {name = 'buffer'},
+                    {name = 'luasnip'},
+                },
             })
         end
     },
