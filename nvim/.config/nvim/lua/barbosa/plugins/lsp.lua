e42ff74e9333b855c8bea439a5f9961f798cd706
Marco Barbosa <marcobarbosa@outlook.com>
2024-02-18 21:07:54 +0100

    remove duplicate

--- nvim/.config/nvim/lua/barbosa/plugins/lsp.lua
+++ nvim/.config/nvim/lua/barbosa/plugins/lsp.lua
@@ -28,7 +28,6 @@ return {
     {'hrsh7th/cmp-nvim-lsp'},
     {'hrsh7th/cmp-path'},
     {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/cmp-buffer'},
     {
         'L3MON4D3/LuaSnip',
         depends = {'rafamadriz/friendly-snippets'},
